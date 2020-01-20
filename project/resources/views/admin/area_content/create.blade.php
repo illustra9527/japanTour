0738ec6809eca8d3d1b51bb3ef1c34f95a5fbeb2
illustra9527 <yabesoube@gmail.com>
2020-01-20 13:53:29 +0800

    Revert "01/20"
    This reverts commit 74f0d825ea668da51a3f1c4ec2a29b71332e860c.

--- project/resources/views/admin/area_content/create.blade.php
+++ project/resources/views/admin/area_content/create.blade.php
@@ -13,45 +13,67 @@
                 <div class="card-header">地區內容管理</div>
 
                 <div class="card-body">
+                    <a class="btn btn-primary" href="/admin/area_content/create">新增</a>
+                    <hr>
+                    <table id="table" class="table table-striped table-bordered" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th style="width:100px;">標題</th>
+                                <th>內文</th>
+                                <th>價錢</th>
+                                <th>圖片</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <form>
+                                <div class="form-group">
+                                    <select class="form-control">
+                                        <option>Default select</option>
+                                    </select>
+                                </div>
 
-                    <form>
-                        <div class="form-group">
-                            <select class="form-control">
-                                <option>Default select</option>
-                            </select>
-                        </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Title</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1"
+                                        aria-describedby="emailHelp" placeholder="Enter email">
+                                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with
+                                        anyone
+                                        else.</small>
+                                </div>
 
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Title</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1"
-                                aria-describedby="emailHelp" placeholder="Enter Title" name="title">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Text</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1"
+                                        aria-describedby="emailHelp" placeholder="Enter email">
+                                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with
+                                        anyone
+                                        else.</small>
+                                </div>
 
-                        </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Price</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1"
+                                        aria-describedby="emailHelp" placeholder="Enter email">
+                                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with
+                                        anyone
+                                        else.</small>
+                                </div>
 
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Text</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1"
-                                aria-describedby="emailHelp" placeholder="Enter Text" name="text">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Img</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1"
+                                        aria-describedby="emailHelp" placeholder="Enter email">
+                                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with
+                                        anyone
+                                        else.</small>
+                                </div>
 
-                        </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
 
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Price</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1"
-                                aria-describedby="emailHelp" placeholder="Enter Price" name="price">
 
-                        </div>
-
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Img</label>
-                            <input type="file" class="form-control" id="exampleInputEmail1"
-                                aria-describedby="emailHelp" placeholder="Enter Img" name="img">
-
-                        </div>
-
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -66,6 +88,10 @@
     <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
     <script>
-
+        $(document).ready(function() {
+            $('#table').DataTable({
+                "order": [1,"desc"]
+            });
+        });
     </script>
     @endsection
